eebcd1db678b16cb290ec14b8ffdad6c006d608f
Shamita Uma Kandan <154904321+shamita98@users.noreply.github.com>
2026-05-07 20:00:31 +0400

    Update survival_scanb_hiseq_test_actual_vs_preds.R

--- survival_analysis/survival_scanb_hiseq_test_actual_vs_preds.R
+++ survival_analysis/survival_scanb_hiseq_test_actual_vs_preds.R
@@ -2,7 +2,9 @@ library(survminer)
 library(survival)
 library(tidyverse)
 
+#--------------------------------------------------------------------
 # load files
+#--------------------------------------------------------------------
 counts_res = read.csv("C:/Users/User/Documents/master_thesis_project_analysis/pam50_classification_counts/counts_model_evaluation/counts_prediction_results/scanb_hiseq_test_counts_rf_predictions.csv",
                   header = 1, row.names = 1)
 rank_res = read.csv("C:/Users/User/Documents/master_thesis_project_analysis/pam50_classification_ranks/ranks_model_evaluation/ranks_prediction_results/scanb_hiseq_test_ranks_svm_predictions.csv",
@@ -11,7 +13,13 @@ word2vec_res = read.csv("C:/Users/User/Documents/master_thesis_project_analysis/
                   header = 1, row.names = 1)
 
 
+#--------------------------------------------------------------------
 # function to create a survival plot
+#--------------------------------------------------------------------
+#--------------------------------------------------------------------
+# function for creating survival plots based on actual PAM50 labels
+#--------------------------------------------------------------------
+#--------------------------------------------------------------------
 actual_survivalPlot = function(df, survival_event, 
                         alive_indicator, plot_title)
 {
@@ -68,6 +76,12 @@ actual_survivalPlot = function(df, survival_event,
   return(plot)
                     
 }
+
+#--------------------------------------------------------------------
+#--------------------------------------------------------------------
+# function for creating survival plots based on model-predicted PAM50 labels
+#--------------------------------------------------------------------
+#--------------------------------------------------------------------
 
 predicted_survivalPlot = function(df, survival_event, 
                                alive_indicator, plot_title)
@@ -126,8 +140,9 @@ predicted_survivalPlot = function(df, survival_event,
   
 }
 
-
-# survival plot using actual subtype labels
+#--------------------------------------------------------------------
+# Survival plots using actual labels
+#--------------------------------------------------------------------
 actual_label_plot = actual_survivalPlot(df = counts_res, 
                                  survival_event = "overall_survival_event", 
                                  alive_indicator = 0, 
@@ -138,8 +153,9 @@ ggsave(actual_label_plot$plot, filename = "scanb_hiseq_test_actual_labels_surviv
        dpi = 500, height = 7, width = 8)
 
 
-
-# survival plot using counts-logreg labels
+#--------------------------------------------------------------------
+# survival plot using counts-rf labels
+#--------------------------------------------------------------------
 counts_rf_plot = predicted_survivalPlot(df = counts_res, 
                                         survival_event = "overall_survival_event", 
                                         alive_indicator = 0, 
@@ -149,8 +165,9 @@ print(counts_rf_plot)
 ggsave(counts_rf_plot$plot, filename = "scanb_hiseq_test_counts_rf_labels_survival.png", 
        dpi = 300, height = 7, width = 8)
 
-
+#--------------------------------------------------------------------
 # survival plot using rank-svm labels
+#--------------------------------------------------------------------
 rank_svm_plot = predicted_survivalPlot(df = rank_res, 
                                             survival_event = "overall_survival_event", 
                                             alive_indicator = 0, 
@@ -160,8 +177,9 @@ print(rank_svm_plot)
 ggsave(rank_svm_plot$plot, filename = "scanb_hiseq_test_rank_svm_labels_survival.png", 
        dpi = 300, height = 7, width = 8)
 
-
-# survival plot using word2vecpca-svm labels
+#--------------------------------------------------------------------
+# survival plot using word2vec-svm labels
+#--------------------------------------------------------------------
 word2vec_svm_plot = predicted_survivalPlot(df = word2vec_res, 
                                        survival_event = "overall_survival_event", 
                                        alive_indicator = 0, 
